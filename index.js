fe34a4f92ce62ffc9997c146b5129e0a378c743d
mallsk <skollermallikarjun@gmail.com>
2024-09-07 22:12:56 +0530

    Sign in added

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 app.use(express.json());
 
 const users = [];
-const generateToken = () =>{
-    
-}
-app.post("/signup", (req,res)=>{
-    const username = req.body.username;
-    const password = req.body.password;
 
-    users.push({
-        username : username,
-        password : password
-    })
+app.post("/signup", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
-    res.json({
-        message : "Signed in Successfully"
-    })
-})
+  users.push({
+    username: username,
+    password: password,
+  });
 
-app.post("/signin",(req,res)=>{
+  res.json({
+    message: "Signed in Successfully",
+  });
+  console.log(users);
+});
 
-})
+app.post("/signin", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  for (let i = 0; i <= users.length; i++) {
+    if (username == users[i].username && password == users[i].password) {
+      res.json({
+        message: "Login successfully",
+      });
+    } else {
+      res.json({
+        message: "User does not exist",
+      });
+    }
+  }
+});
 
 app.listen(3000);
